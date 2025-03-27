78fb28e2c3c6eed375b6ad39b487a15b3783e7ca
MasterSoul21 <lobovins2109@gmail.com>
2025-03-27 10:23:20 -0400

    Add files via upload
    prorgamas python simples con el modulo turtle

---
+++ programa4..py
@@ -0,0 +1,45 @@
+# Ejercicio 4
+import turtle 
+
+v = turtle.Screen()
+t = turtle.Turtle()
+turtle.bgcolor("yellow")
+t.pensize(10)
+t.pencolor("red")
+
+
+t.up()
+t.right(90)
+t.forward(200)
+t.right(90)
+t.forward(300)
+t.right(90)
+t.forward(400)
+t.down()
+
+t.right(150)
+t.forward(300)
+t.left(120)
+t.forward(300)
+t.right(60)
+t.up()
+t.forward(20)
+t.right(90)
+t.forward(300)
+t.down()
+t.left(180)
+t.forward(300)
+t.right(120)
+t.forward(150)
+t.left(60)
+t.forward(150)
+t.right(120)
+t.forward(300)
+
+
+
+
+
+
+
+
